3690235fbc40f66ca671fa82b2a715cdeac45dce
davi <davijluna@gmail.com>
2023-01-06 00:24:58 -0300

    feat: adicionando função que captura os eventos com efeito target no click

--- timer.js
+++ timer.js
@@ -13,3 +13,31 @@ function getSecond(seconds) {
     timeZone: 'UTC'
   });
 }
+
+document.addEventListener('click', function(e) {
+  const el = e.target;
+  
+  if (el.classList.contains('zerar')) {
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00'
+    relogio.classList.remove('pausado');
+    second = 0;
+  }
+
+  if (el.classList.contains('pausar')) {
+    clearInterval(timer);
+  relogio.classList.add('pausado');
+  }
+
+  if (el.classList.contains('iniciar')) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+  }  
+
+  // if (el.classList.contains('iniciar')) {
+  //   relogio.classList.remove('pausado');
+  //   clearInterval(timer);
+  //   iniciaRelogio();
+  // }  
+});
