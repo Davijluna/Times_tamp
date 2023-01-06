59e29515165fd5cc0fcab099c8e97eecb2da5001
davi <davijluna@gmail.com>
2023-01-06 00:23:03 -0300

    feat: adicionando finção de que cria os segundos

--- timer.js
+++ timer.js
@@ -1,3 +1,15 @@
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
+
+const zerar = document.querySelector('.zerar');
+let second = 0;
+let timer;
+
+function getSecond(seconds) {
+  const data = new Date(seconds * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
